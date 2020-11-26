553aba686cd5ae397c72e1d25bccbbd12ee2f27d
Sukumar2512 <sukku1225@gmail.com>
2020-11-26 11:54:06 -0500

    Add files via upload

---
+++ Examroom.js
@@ -0,0 +1,14 @@
+function fullname(name){
+var name= prompt("Enter your name?");
+if(name !="sukumar"){
+	alert("your name is not registered. Please contact School adminstration to register your name");
+}
+else if(name="null"){
+	alert("Sorry we don't know who you are");
+}
+else if(name = "sukumar"){
+	alert("Hi sukumar");
+}
+}
+fullname();
+
---
+++ script.js
@@ -0,0 +1,5 @@
+var firstName=prompt("Enter your first name");
+var lastName= prompt("Enter your last name");
+var welcomeMessage= firstName  + lastName;
+var popUp1= alert("Hi "+  welcomeMessage  + " Welcome to Examination room"); 
+popUp1;
